59573aa980b2eb98ebcc98eb33de36a9e45286e4
himzz1234 <himanshuhim1230@gmail.com>
2023-07-02 17:37:49 +0530

    new

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://connectsocialmedia.onrender.com/api",
 });
 
 export default instance;
