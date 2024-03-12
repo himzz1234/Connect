2e6b86230aff1b08287224f43de9dc02726981f8
himzz1234 <himanshuhim1230@gmail.com>
2024-03-12 19:24:26 +0530

    securing the cookies

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -19,9 +19,10 @@ const generateCookie = (id, username, res) => {
   const token = `Bearer ${generateToken(id, username)}`;
 
   res.cookie("access_token", token, {
-    // httpOnly: true,
+    httpOnly: true,
     expires: new Date(Date.now() + 14 * 24 * 3600 * 1000),
     secure: true,
+    domain: "https://connectsocialmedia.onrender.com/",
   });
 };
 
