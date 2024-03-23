fbbee25291e0bb128bb35f9f7844da48e5d9875f
himzz1234 <himanshuhim1230@gmail.com>
2024-03-24 00:02:31 +0530

    feat: typing indicator

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -86,7 +86,7 @@ const sendResetMail = async (req, res) => {
 
   const user = await User.findOne({ email: emailId });
   const url =
-    "http://localhost:3000/resetpassword/" +
+    "httpS://mernsocialmedia.onrender.com/resetpassword/" +
     generateToken(user._id, user.username, "10m");
 
   const body = `<div>
